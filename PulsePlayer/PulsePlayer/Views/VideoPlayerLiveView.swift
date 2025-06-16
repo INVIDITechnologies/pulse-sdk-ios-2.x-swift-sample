45f8884e6b36cd368d2e9439dde3ab4503482367
Selva S <selvamani.s@invidi.com>
2025-06-16 11:00:31 +0530

    PulseSDK linked with proj config file updation. Git ignore file updated Pulse session created with callbacks implemented Event listeners added. Code optimized.

--- PulsePlayer/PulsePlayer/Views/VideoPlayerLiveView.swift
+++ PulsePlayer/PulsePlayer/Views/VideoPlayerLiveView.swift
@@ -5,109 +5,196 @@
 
 import SwiftUI
 import AVKit
+import Combine
+import Pulse
 
 struct VideoPlayerLiveView: View {
     let video: VideoItem
+    @StateObject private var viewModel = PlayerViewModel()
     
-    private var player: AVPlayer? {
-        guard let url = video.contentUrl else { return nil }
-        return AVPlayer(url: url)
-    }
     var body: some View {
         ZStack {
-            if let player = player {
-                ZStack {
-                    VideoPlayer(player: player)
-                        .onAppear {
-                            player.play()
-                        }
-                        .onDisappear {
-                            player.pause()
-                        }
-                        .edgesIgnoringSafeArea(.all)
-                    
-                    VStack {
-                        Spacer()
-                        
-                        VStack(alignment: .leading, spacing: 12) {
-                            
-                            Button(action: handleAdBreakClick) {
-                                Text("Prepare Ads for next break")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .padding(.horizontal)
-                                    .padding(.vertical, 8)
-                                    .background(Color.white.opacity(0.8))
-                                    .foregroundColor(Color.black)
-                                    .cornerRadius(4)
-                            }
-                            
-                            Button(action: handlePlayClick) {
-                                Text("Play Ads")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .padding(.horizontal)
-                                    .padding(.vertical, 8)
-                                    .background(Color.white.opacity(0.8))
-                                    .foregroundColor(Color.black)
-                                    .cornerRadius(4)
-                            }
-                            
-                            Button(action: handleExtendSessionClick) {
-                                Text("Extend Session")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .padding(.horizontal)
-                                    .padding(.vertical, 8)
-                                    .background(Color.white.opacity(0.8))
-                                    .foregroundColor(Color.black)
-                                    .cornerRadius(4)
-                            }
-                            
-                            HStack {
-                                
-                                Spacer()
-                                
-                                Button(action: handleSkipAdClick) {
-                                    Text("Skip Ad")
-                                        .padding(.horizontal)
-                                        .padding(.vertical, 8)
-                                        .background(Color.white.opacity(0.8))
-                                        .foregroundColor(Color.black)
-                                        .cornerRadius(4)
-                                }
-                            }
-                        }
-                        .padding()
-                        .padding(.bottom, 40)
+            videoPlayerView
+            controlsView
+        }
+        .onAppear {
+            viewModel.initializePlayer(from: video)
+        }
+    }
+    
+    private var videoPlayerView: some View {
+        Group {
+            if let player = viewModel.player {
+                VideoPlayer(player: player)
+                    .onAppear {
+                        viewModel.observePlayerReady()
                     }
-                }
+                    .onDisappear {
+                        viewModel.cleanup()
+                    }
+                    .edgesIgnoringSafeArea(.all)
             } else {
-                VStack(spacing: 12) {
-                    Text("Unable to load video")
-                        .font(.headline)
-                }
-                .padding()
+                ProgressView("Loading Video...")
             }
         }
     }
     
-    private func handleAdBreakClick() {
-        print("request ads clicked")
-        // Add logic here
+    private var controlsView: some View {
+        VStack {
+            Spacer()
+            VStack(alignment: .leading, spacing: 12) {
+                controlButton(title: "Prepare Ads for next break", action:  viewModel.handleAdBreakClick)
+                controlButton(title: "Play Ads", action: viewModel.handlePlayAdClick)
+                controlButton(title: "Extend Session", action: viewModel.handleExtendSessionClick)
+                controlButton(title: "Skip Ad", action: viewModel.handleSkipAdClick)
+            }
+        }
+        .padding()
+        .padding(.bottom, 40)
     }
-    
-    private func handlePlayClick() {
-        print("play ads clicked")
-        // Add logic here
-    }
-    
-    private func handleExtendSessionClick() {
-        print("extend session clicked")
-        // Add logic here
-    }
-    
-    private func handleSkipAdClick() {
-        print("Skip ad clicked")
-        // Add logic here
+ 
+    private func controlButton(title: String, action: @escaping () -> Void) -> some View {
+        Button(action: action) {
+            Text(title)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+                .padding(.vertical, 8)
+                .background(Color.white.opacity(0.8))
+                .foregroundColor(Color.black)
+                .cornerRadius(4)
+        }
     }
 }
 
+class PlayerViewModel: NSObject, ObservableObject, OOPulseSessionDelegate {
+    @Published var player: AVPlayer?
+    var playerItem: AVPlayerItem?
+    var timeControlStatusObserver: NSKeyValueObservation?
+    var cancellables = Set<AnyCancellable>()
+    var avPLayerItem: AVPlayerItem?
+    var contentAsset: AVAsset?
+    var adAsset: AVAsset?
+    var videoAd: OOPulseVideoAd?
+    var isSessionExtensionRequested: Bool = false
+    var session: OOPulseSession?
+    var ooContentMetadata: OOContentMetadata?
+    var ooRequestSettings: OORequestSettings?
+    
+    // Player initialized
+    func initializePlayer(from video: VideoItem) {
+        guard let url = video.contentUrl else { return }
+        playerItem = AVPlayerItem(url: url)
+        player = AVPlayer(playerItem: playerItem)
+        loadPulseSession(video: video)
+    }
+    
+    // Player Observer
+    func observePlayerReady() {
+        guard let item = playerItem else { return }
+        item.publisher(for: \.status)
+            .sink { [weak self] status in
+                if status == .readyToPlay {
+                    self?.player?.play()
+                    self?.timeControlStatusObserver = self?.player?.observe(\.timeControlStatus, options: [.initial, .new]) { [self] player, change in
+                            switch player.timeControlStatus {
+                            case .playing:
+                                self?.handlePlaybackStarted()
+                            case .paused:
+                                self?.handlePlaybackPaused()
+                            case .waitingToPlayAtSpecifiedRate:
+                                self?.handleBufferingOrWaiting()
+                            @unknown default:
+                                break
+                            }
+                        }
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    func loadPulseSession(video: VideoItem) {
+        print("Session Config...")
+        // ContentMetaData configuration
+        ooContentMetadata = OOContentMetadata()
+        ooContentMetadata!.category = video.category
+        ooContentMetadata!.tags = video.tags
+        ooContentMetadata!.contentForm = .long
+        ooContentMetadata!.duration = Double(video.contentDuration ?? 0)
+        ooContentMetadata!.identifier = video.contentId
+        // RequestSettings configuration
+        ooRequestSettings = OORequestSettings()
+        ooRequestSettings!.userAgentForThirdPartyRequests = OOUserAgentFormat.IAB
+        ooRequestSettings!.linearPlaybackPositions = video.midrollPositions
+        //Pulse Host setup and Session trigger.
+        OOPulse.setPulseHost("https://pulse-demo.videoplaza.tv",deviceContainer: nil, persistentId: nil)
+            OOPulse.logDebugMessages(true)
+            session = OOPulse.session(with: ooContentMetadata, requestSettings: ooRequestSettings)
+            session?.start(with: self)
+    }
+    
+    // Custom Button Events
+    func handleAdBreakClick() {
+        print("Request ads clicked")
+    }
+    
+    func handlePlayAdClick() {
+        print("Play ads clicked")
+    }
+    
+    func handleExtendSessionClick() {
+        print("Extend session clicked")
+        guard session != nil else { return }
+        isSessionExtensionRequested = true
+    }
+    
+    func handleSkipAdClick() {
+        print("Skip ad clicked")
+    }
+    
+    // Player Events
+    func handlePlaybackStarted() {
+          print("Playback started")
+    }
 
+    func handlePlaybackPaused() {
+          print("Playback paused")
+    }
+
+    func handleBufferingOrWaiting() {
+          print("Buffering or waiting")
+    }
+    
+    func seek(to time: CMTime) {
+        player?.seek(to: time)
+    }
+    
+    // Pulse Session Callbacks
+    func startContentPlayback() {
+        print("startContentPlayback from delegate")
+//        player?.play()
+    }
+    
+    func start(_ adBreak: (any OOPulseAdBreak)!) {
+        print("startAdBreak from delegate")
+    }
+    
+    func startAdPlayback(with ad: (any OOPulseVideoAd)!, timeout: TimeInterval) {
+        print("startAdPlayback from delegate")
+    }
+    
+    func sessionEnded() {
+        print("sessionEnded from delegate")
+    }
+    
+    func illegalOperationOccurredWithError(_ error: (any Error)!) {
+        print("illegalOperationOccurredWithError from delegate")
+    }
+    
+    // Called on onDisappear
+    func cleanup() {
+        cancellables.removeAll()
+        player?.pause()
+        player = nil
+        playerItem = nil
+    }
+}
